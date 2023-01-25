e0bcfdc94fb7d4b2a95c8d30c7d0d58be81ba807
JunYongSun <ssjy4974@naver.com>
2023-01-25 14:52:33 +0900

    Chore : 패키지구조 수정

---
+++ Backend/src/main/java/com/ddockddack/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.ddockddack.domain.member.repository;
+
+import com.ddockddack.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
