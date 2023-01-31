f4c403b1f5ae449a3cbc864fbb099b13ef9c517e
소지현 <s688922@gmail.com>
2023-01-31 13:09:17 +0900

    S08P12A409-240 Design : font 변경

---
+++ frontend/src/components/common/NavigationBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="navigation-bar">
+    <span class="left">
+      <router-link to="/">똑딱</router-link>
+    </span>
+    <span class="mid">
+      <router-link to="/gameList">게임 목록</router-link>
+      <router-link to="/gameMake">게임 만들기</router-link>
+      <router-link to="/bestcut">베스트 컷</router-link>
+    </span>
+    <span class="right">
+      <router-link to="/member">마이 페이지</router-link>
+      <router-link to="/admin">관리자 페이지</router-link>
+    </span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.navigation-bar {
+  background-color: #fdf8ec;
+  height: 95px;
+}
+a {
+  text-decoration: none;
+  color: black;
+  line-height: 95px;
+  margin: 40px;
+}
+.left a {
+  font-family: "Gugi-Regular";
+  font-size: 48px;
+}
+.mid a {
+  font-size: 24px;
+  font-family: "NanumSquareRoundEB";
+}
+.right a {
+  font-size: 24px;
+  font-family: "NanumSquareRoundEB";
+}
+
+.left {
+  float: left;
+}
+
+.mid {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.right {
+  float: right;
+}
+</style>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,17 +1,71 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <!-- <hello-world msg="Welcome to Your Vue.js App" /> -->
+  <div id="view">
+    <div class="box">
+      <div class="logo"><span>똑딱</span></div>
+      <div class="form">
+        <div class="input">게임 PIN</div>
+        <div class="button">입력</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// import HelloWorld from "@/components/HelloWorld.vue";
+<script setup></script>
 
-export default {
-  name: "HomeView",
-  components: {
-    // HelloWorld,
-  },
-};
-</script>
+<style scoped>
+#view {
+  background-color: #fdf8ec;
+  height: calc(100vh - 95px + 1000px);
+  position: relative;
+}
+.box {
+  /* border: 1px solid red; */
+  position: relative;
+  position: absolute;
+  left: 50%;
+  top: calc((100vh - 95px) * 0.5);
+  transform: translate(-50%, -50%);
+}
+
+.logo {
+  font-family: "Gugi", cursive;
+  font-size: 96px;
+  text-align: center;
+}
+
+.form {
+  width: 300px;
+  font-size: 20px;
+  background-color: #f9cf5c;
+  border-radius: 10px;
+  height: 200px;
+  margin: 0 auto;
+}
+
+.input,
+.button {
+  border-radius: 10px;
+  margin: 0 auto;
+}
+
+.input {
+  background-color: white;
+  width: 220px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  position: relative;
+  top: 45px;
+}
+
+.button {
+  background-color: #333333;
+  color: white;
+  width: 220px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  position: relative;
+  top: 55px;
+}
+</style>
