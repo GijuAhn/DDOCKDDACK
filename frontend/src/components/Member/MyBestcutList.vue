c2fcb8b8b2c2c022b0fb5f2f97dd7155986762de
l6778 <l6778@naver.com>
2023-02-02 01:50:23 +0900

    Feat: Vue Server api 요청 추가

--- frontend/src/components/Member/MyBestcutList.vue
+++ frontend/src/components/Member/MyBestcutList.vue
@@ -4,8 +4,54 @@
 
 <script setup>
 import { useStore } from "vuex";
+import { apiInstance } from "@/api/index";
+import { ref } from "vue";
 
+const api = apiInstance();
 const store = useStore();
+const myGameList = ref();
+
+onCreated(() => {
+  const callApi = () => {
+    api
+      .get(`/members/games`, {
+        headers: {
+          "access-token": access-token // 변수로 가지고있는 AccessToken
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        myGameList.value = response.data.content;
+      })
+      .catch((error) => {
+        if(error.get("code") == 401){ //401 access 만료 에러가 뜨면 
+          getAccessTokenByRefreshToken(); // refresh 토큰으로 다시 
+          this.callApi(); //갱신된 access-token으로 다시 요청
+        } else {
+          console.log(error);
+        }
+      });
+    }
+})
+
+
+//공통 메서드로 만들기 mixin??
+const getByRefreshToken = () => { //AccessToken 만료되면 refresh로
+  api
+    .get(`/token/refresh`, {
+      headers: {
+        "refresh-token": refresh-token
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      myGameList.value = response.data.content;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 
 store.dispatch("commonStore/setMemberTabAsync", 3);
 </script>
