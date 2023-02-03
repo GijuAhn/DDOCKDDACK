f81e11258aea6ebd8293ca6a0716b94da189c247
l6778 <l6778@naver.com>
2023-02-03 17:55:08 +0900

    Design: MyPage Profile 구성

--- frontend/src/components/GameList/item/NormalGame.vue
+++ frontend/src/components/GameList/item/NormalGame.vue
@@ -1,167 +1,42 @@
 <template>
-  <div id="content">
-    <div id="topSection">
-      <img
-        :src="
-          require(`@/../../Backend/images/${props.game.gameId}/${props.game.thumbnail}`)
-        "
-        alt="대표사진"
-        class="image"
-      />
-    </div>
-    <div id="bottomSection">
-      <div id="gameTitle">
-        <span>{{ props.game.gameTitle }}</span>
-      </div>
-      <div id="gameDesc">
-        <span>{{ props.game.gameDesc }}</span>
-      </div>
-      <div id="createRoomButton"><button>방 생성</button></div>
-      <div id="etcSection" v-click-outside-element="onClickOutside">
-        <div id="etcButton" @click="open">
-          <img
-            :src="require(`@/assets/images/etc-button.png`)"
-            alt="대표사진"
-            class="etc"
-          />
-        </div>
-        <div id="etc" v-show="state" class="test">
-          <div><span>즐겨찾기</span></div>
-          <div><span>베스트 컷</span></div>
-          <div @click="test"><span>문제 미리보기</span></div>
-          <div><span>신고</span></div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <img
+      :src="`${GAMEIMAGES_PATH}/${props.game.gameId}/${props.game.thumbnail}`"
+      alt="대표사진"
+      width="200"
+      height="200"
+    />
+    {{ props.game.gameTitle }}
+    {{ props.game.gameDesc }}
+    {{ props.game.popularity }}
+    <button @click="createSession(props.game.gameId)">방 생성</button>
+    <button>즐겨찾기</button>
+    <button>베스트컷</button>
+    <button>문제 미리보기</button>
+    <button>신고</button>
   </div>
 </template>
 
 <script setup>
-import { defineProps, ref } from "vue";
-import { useStore } from "vuex";
-
-const store = useStore();
+import { defineProps, onMounted } from "vue";
+import { apiInstance } from "@/api/index";
+import router from "@/router/index.js";
+import process from "process";
 
 const props = defineProps(["game"]);
 const api = apiInstance();
 const GAMEIMAGES_PATH = process.env.VUE_APP_GAMEIMAGES_PATH;
 
-const onClickOutside = () => {
-  // console.log(e);
-  state.value = false;
-};
+onMounted(() => {
+  console.log(process.env);
+});
 
-const open = () => {
-  state.value = !state.value;
+const createSession = (gameId) => {
+  api.post("/api/game-rooms", { gameId }).then((res) => {
+    router.replace(`/gameroom/${res.data}`);
+  });
 };
-
-const state = ref(false);
-
-const test = () => {
-  store.dispatch("commonStore/setCurrentModalAsync", "preview");
-};
+// console.log(props.game);
 </script>
 
-<style scoped>
-#content {
-  border: 2px solid black;
-  width: 325px;
-  height: 380px;
-}
-#createRoomButton {
-  margin-top: 10px;
-}
-#gameTitle {
-  width: 95%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-#gameDesc {
-  margin-top: 5px;
-  width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-#createRoomButton button {
-  border: 1px solid #da6c6b;
-  background-color: white;
-  font-size: 14px;
-  font-family: "NanumSquareRoundB";
-  width: 100%;
-  padding: 10px 0;
-  color: #da6c6b;
-}
-#createRoomButton button:hover {
-  color: white;
-  background-color: #da6c6b;
-  cursor: pointer;
-  transition: 0.3s;
-}
-#gameTitle span {
-  font-size: 22px;
-}
-#gameDesc span {
-  font-size: 16px;
-  color: #656565;
-}
-.image {
-  width: 325px;
-  height: 260px;
-  object-fit: cover;
-}
-#bottomSection {
-  padding: 5px 10px;
-  position: relative;
-}
-#etcButton {
-  position: absolute;
-  top: 0px;
-  left: 290px;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  text-align: center;
-}
-#etcButton:hover {
-  background-color: #d9d9d9;
-  cursor: pointer;
-  transition: 0.3s;
-}
-.etc {
-  width: 50%;
-  height: 50%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-/* #content:hover {
-  box-shadow: 0 0 20px #8b8b8b;
-  transition: 0.3s;
-} */
-#etc {
-  position: absolute;
-  top: 30px;
-  left: 290px;
-  background-color: white;
-  width: 130px;
-  box-shadow: 0 0 10px #8b8b8b;
-  z-index: 1;
-  border-radius: 10px;
-  padding: 10px 0;
-  display: block;
-}
-#etc :hover {
-  cursor: pointer;
-}
-#etc span {
-  font-size: 16px;
-  display: block;
-  padding: 10px;
-}
-#etc span:hover {
-  background-color: #d9d9d9;
-}
-</style>
+<style></style>
--- frontend/src/components/common/MyProfile.vue
+++ frontend/src/components/common/MyProfile.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="user-info">프로필 사진, 닉네임, 이메일</div>
+  <div class="user-info">
+    <div id="profile">
+      <div id="profileImg" style="float: left">
+        <img
+          :src="require(`@/../../Backend/images/profile/1/abc2.jpg`)"
+          alt="대표사진"
+          class="image"
+        />
+      </div>
+    </div>
+    <div id="nameAndEmail">
+      <div id="nickname">이름</div>
+      <div id="email">이메일</div>
+      <!-- ${{ props.profile.gameId }}, ${{ props.profile.gameId }} -->
+    </div>
+  </div>
 </template>
 
 <script></script>
 
 <style scoped>
+img {
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  margin-left: auto;
+  margin-right: auto;
+}
 .user-info {
   height: 290px;
+  display: table;
+}
+.user-info > div {
+  display: table-cell;
+  vertical-align: middle;
+}
+#profileImg {
+  margin-right: 30px;
+}
+#nickname {
+  font-size: 48px;
+  margin-bottom: 10px;
 }
 </style>
