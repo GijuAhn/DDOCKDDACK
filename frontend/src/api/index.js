d054ea6e9debb3d354aa5cc2b3e1db52ef215aa0
이해건 <rjslzoq@naver.com>
2023-02-06 14:10:30 +0900

    Refactor: baseURL 삭제

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // local vue api axios instance
 function apiInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:8080", //env로 설정 해줘야함
+    // baseURL: "http://localhost:8080", //env로 설정 해줘야함
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
