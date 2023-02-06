bbd3a5eece5b6b2bb3c862989d05bd202f8bfb24
소지현 <s688922@gmail.com>
2023-02-06 15:10:54 +0900

    Refactor : 경로 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,9 +4,9 @@ import axios from "axios";
 function apiInstance() {
   const instance = axios.create({
     // baseURL: "http://localhost:8080", //env로 설정 해줘야함
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
+    // headers: {
+    //   "Content-Type": "application/json;charset=utf-8",
+    // },
     // headers: {
     //   "Content-Type": "multipart/form-data", //넣어도 되고 안넣어도 되고
     // },
