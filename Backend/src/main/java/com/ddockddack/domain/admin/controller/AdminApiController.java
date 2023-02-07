8641a8fbb2ea886a5a8c3f5c0ac5b692a84f4036
이해건 <rjslzoq@naver.com>
2023-02-07 14:23:11 +0900

    S08P12A409-106 S08P12A409-247 Feat : 신고 된 게임 목록 조회, 신고 된 베스트 컷 목록 조회

--- Backend/src/main/java/com/ddockddack/domain/admin/controller/AdminApiController.java
+++ Backend/src/main/java/com/ddockddack/domain/admin/controller/AdminApiController.java
@@ -1,8 +1,13 @@
 package com.ddockddack.domain.admin.controller;
 
+import com.ddockddack.domain.bestcut.response.BestcutRes;
 import com.ddockddack.domain.bestcut.service.BestcutService;
 import com.ddockddack.domain.game.response.ReportedGameRes;
 import com.ddockddack.domain.game.service.GameService;
+import com.ddockddack.domain.member.controller.TokenController;
+import com.ddockddack.domain.member.service.MemberService;
+import com.ddockddack.domain.member.service.TokenService;
+import com.ddockddack.domain.report.entity.ReportedBestcut;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -24,13 +29,21 @@ public class AdminApiController {
 
     private final GameService gameService;
 
+    private final TokenService tokenService;
+    private final MemberService memberService;
+
     @GetMapping()
     @Operation(summary = "신고된 게임 목록 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "신고된 게임 목록 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "허가되지 않은 사용자"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
-    public ResponseEntity<?> reportedGameList(@RequestHeader(value = "access-token", required = false) String accessToken) {
+    public ResponseEntity<?> reportedGameList(@RequestHeader(value = "access-token", required = true) String accessToken) {
+
+        if(!memberService.isAdmin(tokenService.getUid(accessToken))){
+            return ResponseEntity.status(403).body("");
+        }
 
         try {
             List<ReportedGameRes> allReportedGames = gameService.findAllReportedGames();
@@ -40,5 +53,24 @@ public class AdminApiController {
         }
 
     }
+    @GetMapping()
+    @Operation(summary = "신고된 베스트 컷 목록 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "신고된 베스트 컷 목록 조회 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    public ResponseEntity<?> reportedBestCutList(@RequestHeader(value = "access-token", required = true) String accessToken) {
 
+        if(!memberService.isAdmin(tokenService.getUid(accessToken))){
+            return ResponseEntity.status(403).body("");
+        }
+
+        try {
+            List<ReportedBestcut> allReportedBestCuts = bestcutService.findAllReportedBestCuts();
+            return ResponseEntity.ok(allReportedBestCuts);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e);
+        }
+
+    }
 }
