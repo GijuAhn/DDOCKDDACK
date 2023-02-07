e0a5274b8a0518f96a38b839def61234dc07d5ca
이해건 <rjslzoq@naver.com>
2023-02-07 14:43:50 +0900

    S08P12A409-103 Feat: 신고된 게임 삭제

--- Backend/src/main/java/com/ddockddack/domain/admin/controller/AdminApiController.java
+++ Backend/src/main/java/com/ddockddack/domain/admin/controller/AdminApiController.java
@@ -1,5 +1,6 @@
 package com.ddockddack.domain.admin.controller;
 
+import com.ddockddack.domain.admin.service.AdminService;
 import com.ddockddack.domain.bestcut.response.BestcutRes;
 import com.ddockddack.domain.bestcut.service.BestcutService;
 import com.ddockddack.domain.game.response.ReportedGameRes;
@@ -26,13 +27,10 @@ import java.util.*;
 public class AdminApiController {
 
     private final BestcutService bestcutService;
-
     private final GameService gameService;
+    private final AdminService adminService;
 
-    private final TokenService tokenService;
-    private final MemberService memberService;
-
-    @GetMapping()
+    @GetMapping("/reported/games")
     @Operation(summary = "신고된 게임 목록 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "신고된 게임 목록 조회 성공"),
@@ -41,7 +39,8 @@ public class AdminApiController {
     })
     public ResponseEntity<?> reportedGameList(@RequestHeader(value = "access-token", required = true) String accessToken) {
 
-        if(!memberService.isAdmin(tokenService.getUid(accessToken))){
+        // admin 확인
+        if(!adminService.isAdminByAccessToken(accessToken)){
             return ResponseEntity.status(403).body("");
         }
 
@@ -53,15 +52,17 @@ public class AdminApiController {
         }
 
     }
-    @GetMapping()
+    @GetMapping("/reported/bestcuts")
     @Operation(summary = "신고된 베스트 컷 목록 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "신고된 베스트 컷 목록 조회 조회 성공"),
+            @ApiResponse(responseCode = "403", description = "허가되지 않은 사용자"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
     public ResponseEntity<?> reportedBestCutList(@RequestHeader(value = "access-token", required = true) String accessToken) {
 
-        if(!memberService.isAdmin(tokenService.getUid(accessToken))){
+        // admin 확인
+        if(!adminService.isAdminByAccessToken(accessToken)){
             return ResponseEntity.status(403).body("");
         }
 
@@ -71,6 +72,29 @@ public class AdminApiController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body(e);
         }
+
+    }
+
+    @DeleteMapping("/remove/game/{gameId}")
+    @Operation(summary = "신고된 게임 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "게임 삭제 성공"),
+            @ApiResponse(responseCode = "401", description = "권한 없음"),
+            @ApiResponse(responseCode = "403", description = "허가되지 않은 사용자"),
+            @ApiResponse(responseCode = "404", description = "존재 하지 않는 게임"),
+            @ApiResponse(responseCode = "404", description = "존재 하지 않는 유저")
+    })
+    public ResponseEntity gameRemove(@PathVariable Long gameId,
+                                     @RequestHeader(value = "access-token", required = true) String accessToken) {
+
+        // admin 확인
+        if(!adminService.isAdminByAccessToken(accessToken)){
+            return ResponseEntity.status(403).body("");
+        }
+
+        gameService.removeGame(1L, gameId);
+
+        return ResponseEntity.ok().build();
 
     }
 }
--- Backend/src/main/java/com/ddockddack/domain/admin/service/AdminService.java
+++ Backend/src/main/java/com/ddockddack/domain/admin/service/AdminService.java
@@ -1,10 +1,16 @@
 package com.ddockddack.domain.admin.service;
 
+import com.ddockddack.domain.member.service.MemberService;
+import com.ddockddack.domain.member.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-
+    private final TokenService tokenService;
+    private final MemberService memberService;
+    public boolean isAdminByAccessToken(String accessToken){
+        return memberService.isAdmin(tokenService.getUid(accessToken));
+    }
 }
