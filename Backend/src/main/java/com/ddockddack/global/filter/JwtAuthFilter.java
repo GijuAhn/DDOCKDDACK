50819fcc5cd5adc596c7fb9d770bb67c134fe295
이해건 <rjslzoq@naver.com>
2023-02-10 16:21:47 +0900

    S08P12A409-299 Refactor: filter

--- Backend/src/main/java/com/ddockddack/global/filter/JwtAuthFilter.java
+++ Backend/src/main/java/com/ddockddack/global/filter/JwtAuthFilter.java
@@ -1,6 +1,7 @@
-package com.ddockddack.domain.member.oauth;
+package com.ddockddack.global.filter;
 
 import com.ddockddack.domain.member.entity.Member;
+import com.ddockddack.domain.member.oauth.Token;
 import com.ddockddack.domain.member.repository.MemberRepository;
 import com.ddockddack.domain.member.response.MemberAccessRes;
 import com.ddockddack.domain.member.service.TokenService;
@@ -33,9 +34,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain)
         throws IOException, ServletException {
+
         log.info("Filter 진입");
         log.info("요청 타입 {}", request.getMethod());
         log.info("요청 타입 uri {}", request.getRequestURI());
+
         String accessToken = (request).getHeader("access-token");
         String refreshToken = null;
 
@@ -55,40 +58,56 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         log.info("accessToken {} ", accessToken);
         log.info("refreshToken {} ", refreshToken);
 
-        if (!(accessToken == null || "".equals(accessToken)) && tokenService.verifyToken(accessToken)) {
+        //엑세스 토큰이 없을때
+        if(accessToken == null || "".equals(accessToken)){
+
+            if (refreshToken != null && tokenService.verifyToken(refreshToken)){
+
+                //refresh-token을 받음 access-token 재발급
+                Long id = tokenService.getUid(refreshToken);
+                Token token = tokenService.reGenerateAccessToken(id, "USER", refreshToken);
+                Member member = memberRepository.getReferenceById(id);
+
+                if (member == null) {
+                    throw new NotFoundException(ErrorCode.MEMBER_NOT_FOUND);
+                }
+
+                MemberAccessRes memberAccessRes = new MemberAccessRes(token.getToken(), member.getId());
+
+                Authentication auth = getAuthentication(memberAccessRes);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+            } else if ((request.getMethod().equals("POST") || request.getMethod().equals("PUT"))
+                    && request.getRequestURI().contains("api/game-room")){
+
+                    filterChain.doFilter(request, response);
+                    return;
+
+            } else if (!request.getMethod().equals("GET")){
+
+                log.info(String.valueOf(ErrorCode.EXPIRED_ACCESSTOKEN));
+                throw new AccessDeniedException(ErrorCode.EXPIRED_ACCESSTOKEN);
+
+            }
+
+        } else {
             Long id = tokenService.getUid(accessToken);
             Member member = memberRepository.getReferenceById(id);
 
-            if (member == null) {
-                throw new NotFoundException(ErrorCode.MEMBER_NOT_FOUND);
+            if(tokenService.verifyToken(accessToken)){
+
+                if (member == null) {
+                    throw new NotFoundException(ErrorCode.MEMBER_NOT_FOUND);
+                }
+
+                MemberAccessRes memberAccessRes = new MemberAccessRes(accessToken, member.getId());
+
+                Authentication auth = getAuthentication(memberAccessRes);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
             }
-
-            MemberAccessRes memberAccessRes = new MemberAccessRes(accessToken, member.getId());
-
-            Authentication auth = getAuthentication(memberAccessRes);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-        } else if (refreshToken != null && tokenService.verifyToken(refreshToken)) { //access-token으로 먼저 접근한 경우 클라이언트에 refresh 토큰 보내달라고 알림
-            //refresh-token을 받음 access-token 재발급
-            Long id = tokenService.getUid(refreshToken);
-            Token token = tokenService.reGenerateAccessToken(id, "USER", refreshToken);
-            Member member = memberRepository.getReferenceById(id);
-
-            if (member == null) {
-                throw new NotFoundException(ErrorCode.MEMBER_NOT_FOUND);
-            }
-
-            MemberAccessRes memberAccessRes = new MemberAccessRes(token.getToken(), member.getId());
-
-            Authentication auth = getAuthentication(memberAccessRes);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-
-        }else if((request.getMethod().equals("POST") || request.getMethod().equals("PUT")) && request.getRequestURI().contains("api/game-room")){
-            filterChain.doFilter(request, response);
-            return;
-        }else if(!request.getMethod().equals("GET")){ //GET Mapping이 아닌 경우만 ERROR처리 하지만
-            log.info(String.valueOf(ErrorCode.EXPIRED_ACCESSTOKEN));
-            throw new AccessDeniedException(ErrorCode.EXPIRED_ACCESSTOKEN);
         }
+
         filterChain.doFilter(request, response);
     }
 
