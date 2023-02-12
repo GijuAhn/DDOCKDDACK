612b60f148b54b6e57497a55cc600d88b08b127f
Giju Ahn <60786941+GijuAhn@users.noreply.github.com>
2023-02-13 04:29:57 +0900

    Refactor: 중간결과 점수 스케일링

--- Backend/src/main/java/com/ddockddack/domain/gameRoom/repository/GameMember.java
+++ Backend/src/main/java/com/ddockddack/domain/gameRoom/repository/GameMember.java
@@ -22,14 +22,46 @@ public class GameMember implements Subject {
     private Member member;
     private String nickname;
     private Integer roundScore = 0;
+    private Integer scaledRoundScore = 0;
     private Integer totalScore = 0;
     private String clientIp;
     private List<byte[]> images = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+    private String message;
 
     public GameMember(String socketId, Member member, String nickname, String clientIp) {
         this.socketId = socketId;
         this.member = member;
         this.nickname = nickname;
         this.clientIp = clientIp;
+        this.observers = new ArrayList<>();
+        this.message = "observer registered";
+    }
+
+    @Override
+    public void register(Observer obj) {
+        if (obj == null) throw new NullPointerException("Null Observer");
+        if (!observers.contains(obj)) observers.add(obj);
+    }
+
+    @Override
+    public void unregister(Observer obj) {
+        observers.remove(obj);
+    }
+
+    @Override
+    public void notifyObservers() {
+        this.observers.forEach(Observer::update);
+    }
+
+    @Override
+    public Object getUpdate(Observer obj) {
+        return this.message;
+    }
+
+    public void postMessage(String msg) {
+        System.out.println("GameMember: " + msg);
+        this.message = msg;
+        notifyObservers();
     }
 }
--- Backend/src/main/java/com/ddockddack/domain/gameRoom/repository/RoundObserver.java
+++ Backend/src/main/java/com/ddockddack/domain/gameRoom/repository/RoundObserver.java
@@ -3,9 +3,8 @@ package com.ddockddack.domain.gameRoom.repository;
 public class RoundObserver implements Observer {
 
     private int memberCounter;
-
     private final String name;
-    private Subject gameMember;
+    private final Subject gameMember;
 
     public RoundObserver(String name, Subject gameMember){
         this.name = name;
@@ -16,34 +15,8 @@ public class RoundObserver implements Observer {
     public void update() {
         memberCounter++;
         String msg = (String) gameMember.getUpdate(this);
-        System.out.println("Observer " + name + " got message >> " + msg);
+        System.out.println("Observer " + name + " got message: " + msg);
         System.out.println("Observer counts member... : " + memberCounter);
     }
-
-    /*
-    * GameMember gameMember = new GameMember();
-    * Observer o = new RoundObserver("GameID", gameMember);
-    * gameMember.register(o)
-    * */
-
-//    public RoundObserver(Observable subject) {
-//        subject.addObserver((java.util.Observer) this);
-//    }
-
-//    @Override
-//    public void update(Observable o, Integer score) {
-//        if (o instanceof GameMember) {
-//            GameMember member = (GameMember) o;
-//            memberCounter++;
-//        }
-//        display();
-//    }
-
-//    public boolean isAllMemberScored() {
-//        GameRoom gameRoom;
-//        if (memberCounter == gameRoom.getMembers().size()) {
-//            return true;
-//        }
-//    }
 
 }
