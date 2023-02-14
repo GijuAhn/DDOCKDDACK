2802d33d8c279284c1cf63ac2d4fc37c3b34a920
소지현 <s688922@gmail.com>
2023-02-14 14:43:01 +0900

    S08P12A409-370 Design : 최종결과 모달 띄우기

---
+++ frontend/src/components/common/modal/FinalResultModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="white-bg">
+    <!-- <div v-if="result.length" id="rankContainer"></div> -->
+    <div>아녕하세요!</div>
+  </div>
+</template>
+
+<script setup>
+// import { ref } from "vue";
+// import { computed } from "vue";
+// import { useStore } from "vuex";
+
+// const store = useStore();
+// const currentModal = computed(() => store.state.commonStore.currentModal);
+// const result = ref(currentModal.value.data);
+</script>
+
+<style scoped>
+.white-bg {
+  background-color: white;
+  width: 600px;
+  height: 750px;
+  border-radius: 10px;
+  position: relative;
+}
+</style>
--- frontend/src/views/GameroomView.vue
+++ frontend/src/views/GameroomView.vue
@@ -73,7 +73,9 @@
                   <button @click="setCurrentModalAsync(`bestcutUpload`)">
                     내 사진 보기
                   </button>
-                  <button>최종 결과</button>
+                  <button @click="setCurrentModalAsync(`finalResult`)">
+                    최종 결과
+                  </button>
                 </div>
               </div>
             </div>
@@ -280,7 +282,7 @@ onBeforeMount(() => {
           resultMode.value = false;
           result.value.length = 0;
           setCurrentModalAsync(""); //주석
-          if (round.value < 5 && isHost.value) {
+          if (round.value < 1 && isHost.value) {
             api.get(`/api/game-rooms/${route.params.pinNumber}/round`);
           } else if (isHost.value) {
             api.get(`/api/game-rooms/${route.params.pinNumber}/final`);
@@ -293,17 +295,7 @@ onBeforeMount(() => {
         isEnd.value = true;
         backgroundAudio.pause();
         backgroundAudio.currentTime = 0;
-        const result = await JSON.parse(signal.data);
-        result.forEach((id, index) => {
-          if (openviduInfo.value.session.connection.connectionId === id) {
-            winner.value.splice(index, 0, openviduInfo.value.publisher);
-          } else {
-            const sub = openviduInfo.value.subscribers.find(
-              (s) => s.stream.connection.connectionId === id
-            );
-            winner.value.splice(index, 0, sub);
-          }
-        });
+        result.value = await JSON.parse(signal.data);
         setTimeout(() => {
           resultMode.value = false;
         }, 5000);
@@ -500,6 +492,11 @@ const setCurrentModalAsync = (what) => {
       name: what,
       data: [round, result, isEnd, winner, openviduInfo.value.publisher],
     });
+  } else if (what === "finalResult") {
+    store.dispatch("commonStore/setCurrentModalAsync", {
+      name: what,
+      data: result,
+    });
   } else if (what === "") {
     store.dispatch("commonStore/setCurrentModalAsync", "");
   }
@@ -514,7 +511,7 @@ const pubAudioOff = (video) => {
   video.publishAudio(isPubAudioEnable);
 };
 
-// setCurrentModalAsync("intermediateResult"); //주석
+setCurrentModalAsync("finalResult"); //주석
 </script>
 
 <style scoped>
