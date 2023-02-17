c3dc6636950e3f6dd9b171e4a72727ed4c819602
JunYongSun <ssjy4974@naver.com>
2023-02-17 10:51:33 +0900

    Refactor : springAsyncConfig 수정

--- Backend/src/main/java/com/ddockddack/global/config/SpringAsyncConfig.java
+++ Backend/src/main/java/com/ddockddack/global/config/SpringAsyncConfig.java
@@ -15,7 +15,7 @@ public class SpringAsyncConfig {
     @Bean(name = "threadPoolTaskExecutor")
     public Executor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(3); // 기본 스레드 수
+        taskExecutor.setCorePoolSize(7); // 기본 스레드 수
         taskExecutor.setMaxPoolSize(100); // 최대 스레드 수
         taskExecutor.setQueueCapacity(150); // Queue 사이즈
         taskExecutor.setThreadNamePrefix("Executor-");
